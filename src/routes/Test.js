26bed41fa708477160ff2ab1647e59ff85ef922a
DongWon Sehr <ddoro36@gmail.com>
2023-01-27 15:54:40 +0900

    create test page

---
+++ src/routes/Test.js
@@ -0,0 +1,13 @@
+import MenuBar from "../components/MenuBar";
+
+function Test() {
+    return (
+        <div>
+            <MenuBar />
+            <h1>Test</h1>
+            <p>This is a test page</p>
+        </div>
+    );
+}
+
+export default Test;
